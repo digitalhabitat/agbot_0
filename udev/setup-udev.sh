c60198942dcdc40f7a5965dfe6cdf14f7279b252
Michael Miller <miketm89@gmail.com>
2019-05-14 02:54:29 -0400

    updated /udev readme

--- udev/setup-udev.sh
+++ udev/setup-udev.sh
@@ -1,5 +1,5 @@
 cp ~/catkin_ws/src/agbot_0/udev/*.rules /etc/udev/rules.d/
 echo "udev rules file has been copied to /etc/udev/rules.d/"
 sudo udevadm control --reload-rules && sudo service udev restart && sudo udevadm trigger
-echo "changes should now take effect"
+echo "changes should now take effect... if not you may need to restart the computer"
 
