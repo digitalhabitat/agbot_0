8239164b413f3269683a3cfcb37c5142692a27a6
Michael Miller <miketm89@gmail.com>
2019-05-15 08:00:53 -0400

    added teleop and ackermann to twist scripts

---
+++ scripts/ackermann2twist.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import rospy
+import time
+import math
+import sys
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDrive
+
+
+class Node:
+
+    def __init__(self):
+        rospy.init_node('ackerman_to_twist_node')
+        rospy.on_shutdown(self.shutdown)
+
+        # Parameters
+        self.WHEEL_BASE = float(rospy.get_param("~wheel_base", "1.18")) # distance between front and back tires in meters
+        self.TWIST_COMMAND = rospy.get_param("~twist_command", "roboclaw/cmd_vel")
+        self.ACKERMANN_COMMAND = rospy.get_param("~ackermann_command", "ackermann")
+
+        # Subscribers and Publishers
+        self.ackermann_subscriber = rospy.Subscriber(self.ACKERMANN_COMMAND, AckermannDrive, self.ackermann_callback)
+        self.twist_publisher = rospy.Publisher(self.TWIST_COMMAND, Twist, queue_size="1")
+
+        
+    def run(self):
+        rospy.loginfo("Starting ackerman_to_twist_node")
+        rospy.spin()
+
+
+    def ackermann_callback(self, ackermann):
+        data = Twist()
+
+        # see https://www.me.utexas.edu/~longoria/CyVS/notes/07_turning_steering/07_Turning_Kinematically.pdf
+        data.linear.x = ackermann.speed
+        data.angular.z = (ackermann.speed/self.WHEEL_BASE)*math.tan(ackermann.steering_angle)
+        
+        self.twist_publisher(data)
+
+    def shutdown(self):
+        rospy.logwarn("Shutting down")
+
+if __name__ == "__main__":
+    try:
+        node = Node()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
+    rospy.logware("Exiting")
--- scripts/teleop.py
+++ scripts/teleop.py
@@ -6,36 +6,70 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from ackermann_msgs.msg import AckermannDrive
 
+temp = 0
+
 class Node:
 
     def __init__(self):
         rospy.init_node('joy_telop_node')
         rospy.on_shutdown(self.shutdown)
-        self.joy_subscriber = rospy.Subscriber("joy", Joy, self.joy_callback)
-        self.joy_publisher = rospy.Publisher("cmd_vel", Twist, queue_size="1")
+
+        # Parameters
+        self.MAX_FORWARD_VELOCITY = float(rospy.get_param("~max_forward_vel", "0.3")) # in m/s
+        self.MAX_REVERSE_VELOCITY = float(rospy.get_param("~max_reverse_vel", "0.1")) # in m/s
+        self.MAX_STEERING_ANGLE = float(rospy.get_param("~max_steering_angle", "0.7853982")) # in radians (45 deg)
+        self.ACKERMANN_COMMAND = rospy.get_param("~ackermann_command", "ackermann")
+        self.JOY_MSGS = rospy.get_param("~joy_msgs", "joy")
+
+        # Subscribers and Publishers
+        self.joy_subscriber = rospy.Subscriber(self.JOY_MSGS, Joy, self.joy_callback)
+        self.ackermann_publisher = rospy.Publisher(self.ACKERMANN_COMMAND, AckermannDrive, queue_size="1")
 
         
     def run(self):
-        time.sleep(3) # sleep 1 second
         rospy.loginfo("starting joy_teleop_node")
         rospy.spin()
 
 
-    def joy_callback(self, joy_data):
-        #rospy.loginfo("%d %d %d", joy_data.buttons[0], joy_data.buttons[1], joy_data.buttons[3])
-        data = Twist()
-        #data.linear.x = joy_data.axes[4]
-        #self.joy_publisher.publish(data)
+    def joy_callback(self, joy):
+        global temp
+        data = AckermannDrive()
     
-        # if x button pressed
-        if joy_data.buttons[1]:
-            # if R2 and Not L2
-            if not joy_data.buttons[6]:
-                data.linear.x = joy_data.axes[4] # 1 to -1
-                data.linear.x = 0.5*(-1*(data.linear.x)+1) # 0 to 1
-                self.joy_publisher.publish(data)
-                rospy.loginfo("recv %s", data.linear.x)
+        # x button pressed behaves as a deadman, switch R2 is forward L2 is reverse
 
+        # if x is engadged and L2 and not active
+        if joy.buttons[1] and not joy.buttons[6]:
+            if not joy.buttons[7]:
+                temp = 1
+
+            elif joy.buttons[7] and temp:
+                # if rising edge on R2 drive forward
+                # joy.axes[4] moves from 1 to -1
+                data.speed = self.MAX_FORWARD_VELOCITY*0.5*(-1*(joy.axes[4])+1)
+                # also send steering angle 
+                # joy.axes[0] is left +1 to right -1
+                # right turn should be positive steering angle
+                data.steering_angle = -joy.axes[0]*0.7853981
+                self.ackermann_publisher.publish(data)
+
+         # if x is engadged and R2 and not active
+        elif joy.buttons[1] and not joy.buttons[7]:
+            if not joy.buttons[6]:
+                temp = 1
+                
+            elif joy.buttons[6] and temp:
+                # if rising edge on L2 drive reverse
+                # joy.axes[3] from 1 to -1
+                data.linear.x = self.MAX_REVERSE_VELOCITY*0.5*(joy.axes[3]-1)
+                # also send steering angle 
+                # joy.axes[0] is left +1 to right -1
+                # right turn should be positive steering angle
+                data.steering_angle = -joy.axes[0]*0.7853981
+                self.ackermann_publisher.publish(data)
+
+        # deadman switch activated
+        elif (not joy.buttons[7] and temp) or not joy.buttons[1] or joy.buttons[6] or joy.buttons[0] or (not joy.buttons[6] and temp) or not joy.buttons[0] or joy.buttons[7] or joy.buttons[1]:
+            temp = 0 
 
     def shutdown(self):
         rospy.logwarn("Shutting down")
